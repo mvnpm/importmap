fd33745896d222f24c58385a4f59fd732b4ccfe3
Phillip Kruger <phillip.kruger@gmail.com>
2023-02-13 04:56:04 +1100

    Remove static operations
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- src/main/java/org/mvnpm/importmap/Aggregator.java
+++ src/main/java/org/mvnpm/importmap/Aggregator.java
@@ -3,10 +3,14 @@ package org.mvnpm.importmap;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import org.mvnpm.importmap.model.Imports;
 
 /**
@@ -16,46 +20,94 @@ import org.mvnpm.importmap.model.Imports;
  */
 public class Aggregator {
 
-    private final static ObjectMapper objectMapper = new ObjectMapper();
-    private final static Map<String, String> userProvidedImports = new HashMap<>();
-    private final static Map<String, String> discoveredImports = new HashMap<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final Map<String, String> userProvidedImports = new HashMap<>();
+    private final Set<URL> userProvidedJarUrls = new HashSet<>();
     
-    private Aggregator(){}
+    public Aggregator(){}
     
-    public static Imports aggregate() {
+    public Aggregator(Set<URL> urls){
+        this.userProvidedJarUrls.addAll(urls);
+    }
+    
+    public Aggregator(Map<String, String> importMapings){
+        this.userProvidedImports.putAll(importMapings);
+    }
+    
+    public Aggregator(Set<URL> urls, Map<String, String> importMapings){
+        this.userProvidedJarUrls.addAll(urls);
+        this.userProvidedImports.putAll(importMapings);
+    }
+    
+    public void addMapping(String key, String value){
+        userProvidedImports.put(key, value);
+    }
+    
+    public void addMappings(Map<String, String> all){
+        userProvidedImports.putAll(all);
+    }
+    
+    public void addJarUrl(URL url){
+        userProvidedJarUrls.add(url);
+    }
+    
+    public void addJarUrls(Set<URL> urls){
+        userProvidedJarUrls.addAll(urls);
+    }
+    
+    public Imports aggregate() {
         Map<String, String> allimports = new HashMap<>();
-        allimports.putAll(discoveredImports);
         allimports.putAll(userProvidedImports);
+        allimports.putAll(scanUserProviderUrls());
+        allimports.putAll(scanClassPath()); // TODO: Add boolen to exclude this ?
         return new Imports(allimports);
     }
     
-    public static String aggregateAsJson(){
+    public String aggregateAsJson(){
         try {
-            Imports i = Aggregator.aggregate();
-            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(i);
+            Imports i = aggregate();
+            return this.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(i);
         } catch (JsonProcessingException ex) {
             throw new RuntimeException(ex);
         }
     }
     
-    public static void add(String key, String value){
-        userProvidedImports.put(key, value);
+    public void reset(){
+        this.userProvidedImports.clear();
+        this.userProvidedJarUrls.clear();
     }
     
-    public static void add(Map<String, String> all){
-        userProvidedImports.putAll(all);
+    private Map<String,String> scanUserProviderUrls(){
+        if(!userProvidedJarUrls.isEmpty()){
+            URLClassLoader urlClassLoader = new URLClassLoader(userProvidedJarUrls.toArray(new URL[] {}));
+            try {
+                Enumeration<URL> enumer = urlClassLoader.getResources(Location.IMPORTMAP_PATH);
+                Map<String,String> m = new HashMap<>();
+                while (enumer.hasMoreElements()) {
+                    URL importmapFile = enumer.nextElement();
+                    Imports importForPackage = objectMapper.readValue(importmapFile, Imports.class);
+                    m.putAll(importForPackage.getImports());
+                }
+                return m;
+            }catch (IOException ex) {
+                throw new UncheckedIOException("Could not aggregate importmaps from set of urls", ex);
+            }
+        }
+        return Map.of();
     }
     
-    static {
+    private Map<String,String> scanClassPath(){
         try {
             Enumeration<URL> enumer = Thread.currentThread().getContextClassLoader().getResources(Location.IMPORTMAP_PATH);
+            Map<String,String> m = new HashMap<>();
             while (enumer.hasMoreElements()) {
                 URL importmapFile = enumer.nextElement(); 
                 Imports importForPackage = objectMapper.readValue(importmapFile, Imports.class);
-                discoveredImports.putAll(importForPackage.getImports());
+                m.putAll(importForPackage.getImports());
             }
+            return m;
         } catch (IOException ex) {
-            throw new RuntimeException("Could not aggregate importmaps from classpath", ex);
+            throw new UncheckedIOException("Could not aggregate importmaps from classpath", ex);
         }
     }
 }
